2b699458f14512164f0ec81eedff130d514183ae
Armando Ruiz <armando.ruiz@industrie.co>
2019-11-28 08:20:15 +1100

    199 Creando el formulario para Nuevas Citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import AgregarCita from './components/AgregarCita';
 // REDUX  se importa el store en el app para que el STATE y todas las ACCIONES esten disponible dentro de todos los componenetes de la aplicacion.
 import store from './store';
 import { Provider } from 'react-redux';
@@ -13,7 +13,9 @@ function App() {
           <h1 className="text-center">Administrador de Pacientes de veterinaria</h1>
         </header>
         <div className="row mt-3">
-          <div className="col-md-6">Formulario Aqui</div>
+          <div className="col-md-6">
+            <AgregarCita />
+          </div>
           <div className="col-md-6">Listado Aqui</div>
         </div>
       </div>
